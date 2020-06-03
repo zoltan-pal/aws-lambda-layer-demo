b8f9b0a6e79593779aa43964c94024a78b804610
Zoltan Pal <zoltan.p@outlook.com>
2020-06-03 22:44:59 +0200

    feat: Implement dummy email processor

--- email-processor/src/main/kotlin/com/github/zoltanpal/demo/lambda/email/processor/LambdaEntryPoint.kt
+++ email-processor/src/main/kotlin/com/github/zoltanpal/demo/lambda/email/processor/LambdaEntryPoint.kt
@@ -2,16 +2,25 @@ package com.github.zoltanpal.demo.lambda.email.processor
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.SNSEvent
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.slf4j.LoggerFactory
 
-class LambdaEntryPoint : RequestHandler<Any, Any> {
+class LambdaEntryPoint : RequestHandler<SNSEvent, Void> {
 
     companion object {
         @JvmStatic private val LOGGER = LoggerFactory.getLogger(LambdaEntryPoint::class.java)
     }
 
-    override fun handleRequest(event: Any?, lambdaContext: Context?): Any? {
+    private val objectMapper = jacksonObjectMapper()
+
+    override fun handleRequest(snsEvent: SNSEvent, lambdaContext: Context): Void? {
         LOGGER.info("Processing email...")
+        val rawSnsMessage = snsEvent.records[0].sns.message
+        val emailMessageId = objectMapper.readTree(rawSnsMessage).path("mail").path("messageId").asText()
+        LOGGER.debug("Pretending to do some processing here...")
+        LOGGER.debug("Raw message={}", rawSnsMessage)
+        LOGGER.info("Email processing done for email with messageId={}", emailMessageId)
         return null
     }
 }
